bae273d8d031993c5eaae7a81fe9811bd4c0a0b2
Alondra <asmayen@uamv.edu.ni>
2025-05-15 18:32:50 -0600

    ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -4,20 +4,23 @@ la UAM, durante cinco días de clases. Cada día se tomará asistencia a seis es
 sección. El programa deberá contabilizar y mostrar el total de asistencias registradas por
 sección, así como el total general de la semana. """
 
-# Se crean 3 variables
 asist_A = 0  
 asist_B = 0  
 asist_C = 0  
 
-for dia in range(1, 6): # Significa contar desde el 1 hasta el 5 (el 6 no se incluye). 
-    print(f"Día {dia}")
+for dia in range(1, 6):  # Del día 1 al 5
+    print(f"\nDía {dia}")
 
     # Sección A
     print("Asistencia de la Sección A: ")
     for estudiante in range(1, 7):
         asistio = int(input(f"Estudiante {estudiante} (1=Sí, 0=No): "))
-        if asistio == 1: # Igual: ==
-            asist_A += 1 # Asignar: +=
+        if asistio == 1:
+            asist_A += 1
+        elif asistio == 0:
+            print(f"El estudiante {estudiante} de la Sección A no asistió.")
+        else:
+            print("Respuesta no válida. Debe ingresar 1 (Sí) o 0 (No).")
 
     # Sección B
     print("Asistencia de la Sección B: ")
@@ -25,6 +28,10 @@ for dia in range(1, 6): # Significa contar desde el 1 hasta el 5 (el 6 no se inc
         asistio = int(input(f"Estudiante {estudiante} (1=Sí, 0=No): "))
         if asistio == 1:
             asist_B += 1
+        elif asistio == 0:
+            print(f"El estudiante {estudiante} de la Sección B no asistió.")
+        else:
+            print("Respuesta no válida. Debe ingresar 1 (Sí) o 0 (No).")
 
     # Sección C
     print("Asistencia de la Sección C: ")
@@ -32,10 +39,13 @@ for dia in range(1, 6): # Significa contar desde el 1 hasta el 5 (el 6 no se inc
         asistio = int(input(f"Estudiante {estudiante} (1=Sí, 0=No): "))
         if asistio == 1:
             asist_C += 1
+        elif asistio == 0:
+            print(f"El estudiante {estudiante} de la Sección C no asistió.")
+        else:
+            print("Respuesta no válida. Debe ingresar 1 (Sí) o 0 (No).")
 
-# Mostrar las asistencias
 print("\nResumen de asistencias en la semana:")
 print(f"Total asistencias Sección A: {asist_A}")
 print(f"Total asistencias Sección B: {asist_B}")
 print(f"Total asistencias Sección C: {asist_C}")
-print(f"Total general de asistencias: {asist_A + asist_B + asist_C}") # Asistencia general
+print(f"Total general de asistencias: {asist_A + asist_B + asist_C}")
